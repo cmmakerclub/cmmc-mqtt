6a45022d31f32e80303e3306c00de0489e0a63bb
Nat <nat@cmmc.io>
2017-07-30 15:21:13 +0700

    add example.

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # CMMC MQTT
 
 an easy-way to deal with mqtt
-
-## Development 
-    $ git clone https://github.com/cmmakerclub/gulp-es6-nodejs-template.git your-project
-    $ cd your-project
-    $ yarn
-    $ npm run tdd
-
-##  Production
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,10 @@
-import CONFIG from './conf'
-
 const moment = require('moment-timezone')
 const winston = require('winston')
 const chalk = require('chalk')
 
+console.log(`process.env`, process.env)
 export const logger = new (winston.Logger)({
-  level: CONFIG.LOG_LEVEL,
+  level: process.env.LOG_LEVEL || 'info',
   transports: [
     new (winston.transports.Console)({
       timestamp: () => moment().tz('Asia/Bangkok').format('YYYY-MM-DD HH:mm:ss'),
